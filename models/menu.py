1615b37dfdfe5a9ee338160067d52cd2c741782a
Marco Mansilla <marco@saltalug.org.ar>
2014-09-08 04:21:57 -0300

    menu mejorado, animaciones copadas, falta color y el login esta roto... CTM guille

--- models/menu.py
+++ models/menu.py
@@ -22,18 +22,17 @@ response.google_analytics_id = None
 ## this is the main application menu add/remove items as required
 #########################################################################
 
-if not(session.auth):
-    last_option = (T('Entrar'), ((request.controller=='default')&(request.function=='user')), URL('default', 'user/login'), [])
-else:
-    last_option = (T('Panel'), False, URL('adminweb', 'index'), [])
+#if not(session.auth):
+#      last_option=(A(SPAN(_class='fa fa-sign-in')+" Entrar", ((request.controller=='default')&(request.function=='user')), URL('default', 'user/login'))),
+# else:
+#     last_option=(A(SPAN(_class='fa fa-wrench')+" Panel", ((request.controller=='adminweb')&(request.function=='index')), URL('adminweb', 'index'))),
 
 response.menu = [
-    (T('Inicio'), ((request.controller=='default')&(request.function=='index')), URL('default', 'index'), []),
-    (T('Empresa'), ((request.controller=='default')&(request.function=='empresa')), URL('default', 'empresa'), []),
-    (T('Productos'), ((request.controller=='default')&(request.function=='productos')), URL('default', 'productos'), []),
-#    (T('Servicios'), ((request.controller=='default')&(request.function=='servicios')), URL('default', 'servicios'), []),
-    (A(SPAN(_class='fa fa-envelope'), False, "#myModal", _class="contact")),
-    last_option
+    (A(SPAN(_class='fa fa-home')+" Inicio", ((request.controller=='default')&(request.function=='index')), URL('default', 'index'))),
+    (A(SPAN(_class='fa fa-building')+" Empresa", ((request.controller=='default')&(request.function=='empresa')), URL('default', 'empresa'))),
+    (A(SPAN(_class='fa fa-cubes')+" Productos", ((request.controller=='default')&(request.function=='productos')), URL('default', 'productos'))),
+    (A(SPAN(_class='fa fa-envelope')+" Contacto", False, "#myModal", _class="contact")),
+#    last_option,
 ]
 
 DEVELOPMENT_MENU = False
